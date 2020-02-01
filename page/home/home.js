c93e5069d7a971b7b72400d1352b6fac69a626d8
poker118 <512616779@qq.com>
2020-02-02 00:40:53 +0800

    Day 1

--- page/home/home.js
+++ page/home/home.js
@@ -1,4 +1,29 @@
 Page({
+  data: {
+    name:'Coderwhy',
+    age: 20,
+    students:[
+      { id: 110, name: 'kobe', age: 30 },
+      { id: 111, name: 'james', age: 28 },
+      { id: 112, name: 'lucy', age: 22 },
+      { id: 113, name: 'kate', age: 40 },
+    ],
+    counter: 0
+  },
 
+  ButtonPlus(){
+    //1.错误的做法--界面不会刷新
+    //this.data.counter += 1,
+
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+
+  ButtonMinus(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  }
 })
 
